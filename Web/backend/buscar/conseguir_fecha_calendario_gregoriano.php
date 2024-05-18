2ccebf26db121e370178fc13f840b58e1b3fd609
Mariano Camposeco <mariano1941@outlook.es>
2024-05-18 10:35:11 -0600

    troubleshoot calculadora calendario gregoriano

--- Web/backend/buscar/conseguir_fecha_calendario_gregoriano.php
+++ Web/backend/buscar/conseguir_fecha_calendario_gregoriano.php
@@ -1,7 +1,6 @@
 <?php
 $fecha_base = new DateTime('-3113-08-11');
 $dias_totales = $baktuns * 144000 + $katuns * 7200 + $tuns * 360 + $uinals * 20 + $kins;
-#$fecha_base->add(new DateInterval('P108D'));
 $fecha_base->add(new DateInterval('P' . $dias_totales . 'D'));
 $fecha_gregoriana = $fecha_base->format('Y-m-d');
 $fecha_consultar = $fecha_gregoriana;
--- Web/backend/buscar/conseguir_fecha_cuenta_larga.php
+++ Web/backend/buscar/conseguir_fecha_cuenta_larga.php
@@ -1,73 +1,16 @@
 <?php
-$fecha1 = new DateTime("01-01-2001");
-$fecha2 = new DateTime($fecha_consultar);
-$fecha_actual = strtotime(date("d-m-Y H:i:00", $fecha1->getTimestamp()));
-$fecha_entrada = strtotime($fecha_consultar);
-$diff = $fecha1->diff($fecha2);
+$fecha_base = new DateTime("-3113-08-11");
+$fecha_obtener = new DateTime($fecha_consultar);
+
+$diff = $fecha_base->diff($fecha_obtener);
 $dias = $diff->days;
-$reversa = false;
-if ($fecha_actual > $fecha_entrada) {
-    $reversa = true;
-}
 
-$number_4 = 0;
-if ($dias > 7200) {
-    $number_4 = floor($dias / 7200);
-    $number_3 = floor(($dias % 7200) / 360);
-    $number_2 = floor((($dias % 7200) % 360) / 20);
-    $number_1 = (($dias % 7200) % 360) % 20;
-} else {
-    $number_3 = floor($dias / 360);
-    $number_2 = floor(($dias % 360) / 20);
-    $number_1 = ($dias % 360) % 20;
-}
+$number_4 = floor($dias / 144000);
+$number_3 = floor(($dias % 144000) / 7200);
+$number_2 = floor((($dias % 144000) % 7200) / 360);
+$number_1 = floor(((($dias % 144000) % 7200) % 360) / 20);
+$number_0 = (((($dias % 144000) % 7200) % 360) % 20);
+$fecha_cuenta_larga = strval($number_4) . "." . strval($number_3) . "." . strval($number_2) . "." . strval($number_1) . "." . strval($number_0);
 
-if ($reversa) {
-    $number_1 *= -1;
-    $number_2 *= -1;
-    $number_3 *= -1;
-    $number_4 *= -1;
-}
-
-$number1 = 8 + $number_1;
-$pivot = 0;
-if ($number1 > 19) {
-    $number1 = $number1 - 20;
-    $pivot = 1;
-} elseif ($number1 < 0) {
-    $number1 = 20+ $number1;
-    $pivot = -1;
-}
-
-$number2 = 15 + $number_2 + $pivot; 
-$pivot=0;
-if ($number2 > 17) {
-    $number2 = $number2 - 18;
-    $pivot = 1;
-} elseif ($number2 < 0) {
-    $number2 = 18 + $number2;
-    $pivot = -1;
-}
-$number3 = 7 + $number_3 + $pivot;
-$pivot=0;
-if ($number3 > 19) {
-    $number3 = $number3 - 20;
-    $pivot = 1;
-} elseif ($number3 < 0) {
-    $number3 = 20 + $number3;
-    $pivot = -1;
-}
-$number4 = 19 + $number_4+$pivot;
-$pivot=0;
-if ($number4 > 19) {
-    $number4 = $number4 - 20;
-    $pivot = 1;
-} elseif ($number4 < 0) {
-    $number4 = 20 + $number4;
-    $pivot = -1;
-}
-$number5=12+$pivot;
-
-return strval($number5).".".strval($number4).".".strval($number3).".".strval($number2).".".strval($number1);
-
+return $fecha_cuenta_larga;
 ?>
